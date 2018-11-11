b94b3673399e39a50ce3f54a615082ea8f2835e4
abgata20000 <abgata@abgata.jp>
2018-11-11 14:12:58 +0900

    mod

--- config/environments/review_app.rb
+++ config/environments/review_app.rb
@@ -65,7 +65,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "www_nihongo-chushin_com_#{Rails.env}"
+  # config.active_job.queue_name_prefix = "mention_collection_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
 
